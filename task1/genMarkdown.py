957298fa68398e23ac49ce11db31d8ea0a5f5c7b
Maciej Wiśniewski <maciej.wisniewski.k@gmail.com>
2024-03-04 13:40:20 +0100

    Markdown test 3

--- task1/genMarkdown.py
+++ task1/genMarkdown.py
@@ -1,11 +1,18 @@
 from get_table import get_table
 
 #table = [(int) place, 'name', 'region', 'imgname']
+def genOneElement(element):
+    ret =  f"##{element[0]}. {element[1]}\n"
+    ret += f'###REGION: {element[2]}\n'
+    ret += f'![{element[1]} logo](https://owcdn.net/img/{element[3]})\n'
+    return ret
+
 def genMarkdown(dest_file):
     table = get_table()
     markdown_list = ""
+
     for element in table:
-        markdown_list += f"{element[0]}. {element[1]}\n"
+        markdown_list += genOneElement(element)
     
     with open(dest_file, 'w') as file:
         file.write(markdown_list)
